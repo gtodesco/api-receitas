2fe3d21af62a4c320be66d2a36b68d9d6de5936f
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-12-05 11:54:31 -0300

    feat: Busca receitas
    Busca receitas do site RecipePuppy verificando se o serviço retorna
    erro.

--- src/controllers/RecipeController.js
+++ src/controllers/RecipeController.js
@@ -1,3 +1,15 @@
+const axios = require("axios");
+
+getRecipes = async (ingredients) => {
+    try {
+        const recipes = await axios.get(`http://www.recipepuppy.com/api/?i=${ingredients}`);
+
+        return recipes.data.results;
+    } catch(e) {
+        return {erro: "Recipe service unavailable."}
+    }
+}
+
 module.exports = {
     async getRecipes(req, res) {
         try {
@@ -6,15 +18,20 @@ module.exports = {
                 throw "No ingredient.";
             }
 
-            const ingredients = req.query.i.split(",").sort();
+            const ingredients = req.query.i;
+            const arrIngredients = ingredients.split(",").sort();
             
-            if (ingredients.length > 3) {
+            if (arrIngredients.length > 3) {
                 throw "Informed more than 3 ingredients.";
             }
 
+            let recipes = await getRecipes(ingredients);
 
+            if (recipes.erro) {
+                throw recipes.erro;
+            }
 
-            return res.json(ingredients);
+            return res.json({ingredients, arrIngredients, recipes});
 
         } catch(erro) {
             return res.status(400).json({erro});
